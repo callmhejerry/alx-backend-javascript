daa6222be30e3482bd2fd0f1f581628e584615db
chinedu jeremiah <chinedujeremiah2002@gmail.com>
2023-06-27 01:42:29 +0000

    hot fix

--- 0x00-ES6_basic/9-getFullBudget.js
+++ 0x00-ES6_basic/9-getFullBudget.js
@@ -4,11 +4,11 @@ export default function getFullBudgetObject(income, gdp, capita) {
   const budget = getBudgetObject(income, gdp, capita);
   const fullBudget = {
     ...budget,
-    getIncomeInDollars(inc) {
-      return `$${inc}`;
+    getIncomeInDollars(income) {
+      return `$${income}`;
     },
-    getIncomeInEuros(inc) {
-      return `${inc} euros`;
+    getIncomeInEuros(income) {
+      return `${income} euros`;
     },
   };
 
