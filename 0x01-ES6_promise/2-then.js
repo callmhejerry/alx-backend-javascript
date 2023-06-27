e5df0864547f3ef958806ee4bd9521aec9406abb
chinedu jeremiah <chinedujeremiah2002@gmail.com>
2023-06-27 18:32:17 +0100

    hot fix

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,6 +1,9 @@
 export default function handleResponseFromAPI(promise) {
-  promise.resolve({
-    status: 200,
-    body: 'success',
-  }).then(() => { console.log('Got a response from the API'); }).catch(() => Error());
+  promise.then(() => {
+    console.log('Got a response from the API');
+    return {
+      status: 200,
+      body: 'success',
+    };
+  }).catch(() => Error());
 }
