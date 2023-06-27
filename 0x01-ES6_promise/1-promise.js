6be7c5387a128948a7f4ec7dd8793d039fc80919
chinedu jeremiah <chinedujeremiah2002@gmail.com>
2023-06-27 19:27:53 +0100

    Added 2-then.js

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,10 +1,10 @@
-export default function getFullResponseFromAPI(success) {
-  const err = Error('The fake API is not working currently');
-  if (success === true) {
-    return Promise.resolve({
-      status: 200,
-      body: 'Success',
-    });
-  }
-  return Promise.reject(err);
+function getFullResponseFromAPI(success) {
+    const err = Error('The fake API is not working currently');
+    if (success === true) {
+        return Promise.resolve({
+            status: 200,
+            body: 'Success',
+        });
+    }
+    return Promise.reject(err);
 }
