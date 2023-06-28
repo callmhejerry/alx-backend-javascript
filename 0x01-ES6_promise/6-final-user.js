d16666d127f4776d1392cbada19cbae7f8239009
chinedu jeremiah <chinedujeremiah2002@gmail.com>
2023-06-28 05:32:29 +0100

    Added 6-final-user.js

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,21 +1,21 @@
-import signUpUser from './4-user-promise.js';
-import uploadPhoto from './5-photo-reject.js';
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, filename) {
-    const allPromises = [signUpUser(firstName, lastName), uploadPhoto(filename)];
-    const results = [];
-    for (const promise of allPromises) {
-        promise.then((value) => {
-            results.push({
-                value,
-            });
-        });
-        promise.catch((value) => {
-            results.push({
-                value,
-            });
-        });
-    }
-
+  const allPromises = [signUpUser(firstName, lastName), uploadPhoto(filename)];
+  return Promise.allSettled(allPromises).then((values) => {
+    const results = values.map((promise) => {
+      if (promise.status === 'fulfilled') {
+        return {
+          status: promise.status,
+          value: promise.value,
+        };
+      }
+      return {
+        status: promise.status,
+        value: `${promise.reason}`,
+      };
+    });
     return results;
+  });
 }
