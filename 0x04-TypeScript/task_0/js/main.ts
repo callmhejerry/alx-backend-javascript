8abf770d57d846d5affc4850075690121a915926
chinedu jeremiah <chinedujeremiah2002@gmail.com>
2023-12-22 21:33:05 +0100

    Hot fix

--- 0x04-TypeScript/task_0/js/main.ts
+++ 0x04-TypeScript/task_0/js/main.ts
@@ -2,10 +2,10 @@ interface Student {
     firstName: string;
     lastName: string;
     age: number;
-    location : string;
+    location: string;
 }
 
-let student1 : Student = {
+const student1: Student = {
     firstName : "Jeremiah",
     lastName : "Chinedu",
     age: 21,
@@ -13,27 +13,25 @@ let student1 : Student = {
 };
 
 
-let student2 :Student  = {
+const student2: Student  = {
     firstName  : "Jessica",
     lastName : "Obodo",
     age : 24,
     location  : "Enugu"
 };
 
-let studentsList: Student[] = [student1, student2];
+const studentsList: Student[] = [student1, student2];
 
 
-// function renderTable() {
-//     const table = document.createElement('table');
+const table = document.createElement('table');
     
-//     studentsList.forEach(student => {
-//       const row = table.insertRow();
-//       const cell1 = row.insertCell();
-//       const cell2 = row.insertCell();
+    studentsList.forEach(student => {
+      const row = table.insertRow();
+      const cell1 = row.insertCell();
+      const cell2 = row.insertCell();
   
-//       cell1.textContent = student.firstName;
-//       cell2.textContent = student.location;
-//     });
+      cell1.textContent = student.firstName;
+      cell2.textContent = student.location;
+    },);
   
-//     document.body.appendChild(table);
-//   }
+    document.body.appendChild(table);
