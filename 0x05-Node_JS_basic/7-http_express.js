3e1901e390335b882baef77afa914a8b467b1198
chinedu jeremiah <chinedujeremiah2002@gmail.com>
2024-01-29 16:54:18 +0100

    style fix

--- 0x05-Node_JS_basic/7-http_express.js
+++ 0x05-Node_JS_basic/7-http_express.js
@@ -9,17 +9,17 @@ const app = express();
 const port = 1245;
 
 app.get('/', (req, res) => {
-    res.send('Hello Holberton School!');
+  res.send('Hello Holberton School!');
 });
 
 app.get('/students', async (req, res) => {
-    const msg = 'This is the list of our students\n';
-    try {
-        const students = await countStudents(DATABASE);
-        res.send(`${msg}${students.join('\n')}`);
-    } catch (error) {
-        res.send(`${msg}${error.message}`);
-    }
+  const msg = 'This is the list of our students\n';
+  try {
+    const students = await countStudents(DATABASE);
+    res.send(`${msg}${students.join('\n')}`);
+  } catch (error) {
+    res.send(`${msg}${error.message}`);
+  }
 });
 
 app.listen(port, () => { });
